65e275a1491261ee40204f16f71b85a4bf82f9cb
Erald Bulku <erald.b@scopicsoftware.com>
2018-06-05 18:42:46 +0200

    no message

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+const colors = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'purple'
+];
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+app.get('/cards', (req, res) => {
+	res.render('card', {prompt: "Who is buried in Grant's tomb?", hint: "Hint", colors});
+});
+
+app.listen(3000, () => {
+	console.log('App is running on localhost')
+});
